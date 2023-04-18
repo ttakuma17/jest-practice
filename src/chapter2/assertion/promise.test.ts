8b41127a63f84371f9aee248d4ca0b58492dfd93
ttakuma17 <ttanimiya@gmail.com>
2023-04-18 11:48:35 +0900

    taskpromiseの追加

--- src/chapter2/assertion/promise.test.ts
+++ src/chapter2/assertion/promise.test.ts
@@ -19,3 +19,19 @@ doSomeThingAsync().then(successCallBack).catch(failureCallBack);
 
 // thenのみを利用した例
 doSomeThingAsync().then(successCallBack, failureCallBack);
+
+const taskPromise = (name, total) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            total += 1;
+            console.log(`${name} finished! Total is ${total}.`)
+            resolve(total)
+        }, 1000)
+    })
+}
+
+taskPromise("task-1", 0)
+    .then(total => taskPromise("task-2", total))
+    .then(total => taskPromise("task-3", total))
+    .then(total => taskPromise("task-4", total))
+    .then(total => taskPromise("task-5", total))
