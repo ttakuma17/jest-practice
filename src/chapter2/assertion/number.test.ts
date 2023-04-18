565a40ed86103671f6615bf84db744f3984889fe
ttakuma17 <ttanimiya@gmail.com>
2023-04-18 10:53:53 +0900

    数値比較 greater than

--- src/chapter2/assertion/number.test.ts
+++ src/chapter2/assertion/number.test.ts
@@ -1,3 +1,21 @@
+// 浮動小数点
 test("0.1 + 0.2 returns 0.3", () => {
     expect(0.1 + 0.2).toBe(0.3)
 })
+
+// 数値の比較
+// toBeGreaterThan
+it("0.1+0.2 は0.3よりも大きい", () => {
+    expect(0.1 + 0.2).toBeGreaterThan(0.3);
+    expect(0.1 + 0.2 > 0.3).toBe(true);
+
+})
+
+// toBeGreaterThanOrEqual
+it("0.1+0.2は0.3よりも大きい、または、0.3000000000004と等しい", () => {
+    expect(0.1 + 0.2).toBeGreaterThanOrEqual(0.3);
+    expect(0.1 + 0.2).toBeGreaterThanOrEqual(0.30000000000000004);
+    expect(0.1 + 0.2 >= 0.3).toBe(true);
+    expect(0.1 + 0.2 >= 0.30000000000000004).toBe(true);
+})
+
